14fbe5a3c0decb272de216fc3571b8edc7ee251e
Kanishk Khurana <kanishkkhurana152@gmail.com>
2022-09-24 21:09:08 +0530

    blockchain layer added, frontend integration pending

--- smart-contract/migrations/1_initial_migration.js
+++ smart-contract/migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const Voting = artifacts.require("Voting");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Voting);
 };
